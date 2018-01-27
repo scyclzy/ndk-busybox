70c373ff04b1a67713df034acdf39aeeb168b0c1
topjohnwu <topjohnwu@gmail.com>
2018-01-27 23:22:00 +0800

    [PATCH] ash history

--- shell/ash.c
+++ shell/ash.c
@@ -13801,7 +13801,7 @@ procargs(char **argv)
 	}
 	optschanged();
 
-	return login_sh;
+	return 1;
 }
 
 /*
@@ -13921,7 +13921,7 @@ int ash_main(int argc UNUSED_PARAM, char **argv)
 		const char *hp;
 
 		state = 1;
-		read_profile("/etc/profile");
+		read_profile("/system/etc/profile");
  state1:
 		state = 2;
 		hp = lookupvar("HOME");
@@ -13959,6 +13959,9 @@ int ash_main(int argc UNUSED_PARAM, char **argv)
 		if (iflag) {
 			const char *hp = lookupvar("HISTFILE");
 			if (!hp) {
+#ifdef __ANDROID__
+				setvar("HISTFILE", "/mnt/sdcard/ash_history", 0);
+#else
 				hp = lookupvar("HOME");
 				if (hp) {
 					INT_OFF;
@@ -13968,6 +13971,7 @@ int ash_main(int argc UNUSED_PARAM, char **argv)
 					INT_ON;
 					hp = lookupvar("HISTFILE");
 				}
+#endif
 			}
 			if (hp)
 				line_input_state->hist_file = hp;
